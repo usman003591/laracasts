c5fc010b14e383aebb8d329bc7022177314c73c1
usman003591 <usman003591@gmail.com>
2024-11-05 16:35:30 +0500

    Changes added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
 import './bootstrap';
+import '../css/app.css'
 
-// alert("Hello from the laracasts!!!")
+// alert("Hello from the laracasts")
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>laracasts</title>
-    {{-- @vite(['resources/js/app.js','resources/css/app.css']) --}}
+    @vite(['resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     </link>
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 </head>
 
 <body class="h-full">
